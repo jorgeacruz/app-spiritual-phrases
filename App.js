bb8d43cc836f8e64193f35d57b61b95dc5aedd41
jorgeacruz <j.alexcruz@gmail.com>
2022-05-25 17:11:56 -0300

    Update all files

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
-import React from "react";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from './src/pages';
+import Green from './src/pages/greenpage';
 
-import LightPage from "./pages/index";
-import DarkPage from "./pages/dark";
+const Stack = createNativeStackNavigator();
 
- const Stack = createNativeStackNavigator();
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LightPage" component={LightPage}
-          options={{ title: 'Light Theme',
-            headerStyle:{ backgroundColor: '#121212'}, headerShown: false, }}
-          />
-
-<Stack.Screen name="DarkPage" component={DarkPage}
-          options={{ title: 'Dark Theme',
-            headerStyle:{ backgroundColor: '#121212'}, headerShown: false, }}
-          />
-  
+        <Stack.Screen name='Home' component={Home} options={{headerShown:false}}/>
+        <Stack.Screen name='Green' component={Green} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
+
--- README.md
+++
@@ -1,6 +0,0 @@
-# Spiritual Phrases
-An simple and beatiful app to randomize somes phrases.
-
-## Figma Project
- ### https://bit.ly/3wwSMdv
-
